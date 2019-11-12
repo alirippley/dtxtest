538f707bf26c26766126613f16ed5d219dd32219
arippley <arippley777@gmail.com>
2019-11-12 10:55:09 -0600

    test

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -11,34 +11,34 @@ class AdminDashboard extends Component {
 
   render() {
     return (
-      <header id="header" className="alt">
-					<h1 id="logo"><a href="index.html">Twenty <span>by HTML5 UP</span></a></h1>
-					<nav id="nav">
+      	<header id="header" className="alt">
+			<h1 id="logo"><a href="index.html">Twenty <span>by HTML5 UP</span></a></h1>
+			<nav id="nav">
+				<ul>
+					<li className="current"><a href="index.html">Welcome</a></li>
+					<li className="submenu opener active">
+						<a href="#">Layouts</a>
 						<ul>
-							<li className="current"><a href="index.html">Welcome</a></li>
-							<li className="submenu opener active">
-								<a href="#">Layouts</a>
+							<li><a href="left-sidebar.html">Left Sidebar</a></li>
+							<li><a href="right-sidebar.html">Right Sidebar</a></li>
+							<li><a href="no-sidebar.html">No Sidebar</a></li>
+							<li><a href="contact.html">Contact</a></li>
+							<li className="submenu">
+								<a href="#">Submenu</a>
 								<ul>
-									<li><a href="left-sidebar.html">Left Sidebar</a></li>
-									<li><a href="right-sidebar.html">Right Sidebar</a></li>
-									<li><a href="no-sidebar.html">No Sidebar</a></li>
-									<li><a href="contact.html">Contact</a></li>
-									<li className="submenu">
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Dolore Sed</a></li>
-											<li><a href="#">Consequat</a></li>
-											<li><a href="#">Lorem Magna</a></li>
-											<li><a href="#">Sed Magna</a></li>
-											<li><a href="#">Ipsum Nisl</a></li>
-										</ul>
-									</li>
+									<li><a href="#">Dolore Sed</a></li>
+									<li><a href="#">Consequat</a></li>
+									<li><a href="#">Lorem Magna</a></li>
+									<li><a href="#">Sed Magna</a></li>
+									<li><a href="#">Ipsum Nisl</a></li>
 								</ul>
 							</li>
-							<li><a href="#" className="button primary">Sign Up</a></li>
 						</ul>
-					</nav>
-				</header>
+					</li>
+					<li><a href="#" className="button primary">Sign Up</a></li>
+				</ul>
+			</nav>
+		</header>
 
     );
   }
